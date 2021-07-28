2b893873955859775a6b329572ad740afd7e5876
Tien-Thinh <35122794+tttienthinh@users.noreply.github.com>
2021-07-28 23:30:38 +0200

    reddit done

--- replit/main.py
+++ replit/main.py
@@ -1,41 +1,87 @@
-import requests
-import json
-import pickle
-import time
+import pandas as pd
+from datetime import datetime
 from flask import Flask
-from flask import request, jsonify
-
-with open('env.json', 'r') as fp:
-    env = json.load(fp)
-
-URL = env["URL"]
-bot_token = env["BinanceStakeBot"]
-bot_chatID = env["Channel id"]
+from flask import request
+import os
 
 
 app = Flask('')
+target_password = os.getenv("password")
 
-
-@app.route('/', methods=['GET'])
-def home():
-    if 'ip' in request.args:
-        ip = request.args['ip']
-        message = request.args['log']
-
-        with open("data.txt", "a+") as f:
-            f.write(f"{ip} - {message}\n")
-            f.close()
-    return "This Website support the "
-
-
-@app.route('/api/startup/python', methods=['GET'])
-def startup(): # ip -> credit exec
+def calcul_credit(ip):
+    df_fees = pd.read_csv("data/fees.csv")
+    df_pay = pd.read_csv("data/pay.csv")
     
-    return credit, exec
+    fees = df_fees.USDTfees[df_fees.ip == ip].sum()
+    pay = df_pay.USDTpay[df_pay.ip == ip].sum()
+    
+    credits = 1 + pay - fees
+    return float(credits)
 
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
+@app.route('/')
+def home():
+    return "This Website support the <a href='https://tttienthinh.github.io/AutoStake/'>GitHub</a>"
+
+# Startup
+@app.route('/api/startup/', methods=['GET'])
+def startup(): # ip version -> credit exec
+    ip = request.args['ip']
+    version = request.args['version']
+
+    with open("data/startup.csv", "a+") as f:
+        f.write(f"\n{ip},{datetime.now()},{version}")
+        f.close()    
+
+    credit = calcul_credit(ip)
+    exec = "None"
+    return {"credit": credit, "exec": exec}
+
+# Log
+@app.route('/api/log/', methods=['GET'])
+def log(): # ip version log -> credit exec
+    ip = request.args['ip']
+    version = request.args['version']
+    log = request.args['log']
+
+    with open("data/log.csv", "a+") as f:
+        f.write(f"\n{ip},{datetime.now()},{version},{log}")
+        f.close()    
+
+    exec = "None"
+    return {"exec": exec}
+
+# Fees
+@app.route('/api/fees/', methods=['GET'])
+def fees(): # ip token token_amount USDT_amount USDT_fees -> exec
+    ip = request.args['ip']
+    token = request.args['token']
+    token_amount = request.args['token_amount']
+    USDT_amount = request.args['USDT_amount']
+    USDT_fees = request.args['USDT_fees']
+
+    with open("data/fees.csv", "a+") as f:
+        f.write(f"\n{ip},{datetime.now()},{token},{token_amount},{USDT_amount},{USDT_fees}")
+        f.close()    
+
+    exec = "None"
+    return {"exec": exec}
+
+# Pay
+@app.route('/api/pay/', methods=['GET'])
+def pay(): # ip email  USDT_pay pass -> credit
+    ip = request.args['ip']
+    email = request.args['email']
+    USDT_pay = request.args['USDT_pay']
+    password = request.args['password']
+
+    if password == target_password:
+    with open("data/pay.csv", "a+") as f:
+        f.write(f"\n{ip},{datetime.now()},{email},{USDT_pay}")
+        f.close()    
+
+    credit = calcul_credit(ip)
+    return {"credit": credit}
 
 
-run()
+app.run(host='0.0.0.0', port=8080, debug=True)
